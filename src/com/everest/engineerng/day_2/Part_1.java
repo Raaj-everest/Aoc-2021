d0b118208752910fde3542a36f054fb0b640f9c0
rajesh <raaj.everest@gmail.com>
2021-12-02 12:03:22 +0530

    Completd part-1 of Day-2

---
+++ src/com/everest/engineerng/day_2/Part_1.java
@@ -0,0 +1,38 @@
+package com.everest.engineerng.day_2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Part_1 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // write your code here
+        Scanner in = new Scanner(new FileReader("/Users/raaj/projects/aocDay-2/src/com/everest/engineerng/input.txt"));
+        int forward = 0;
+        double depth = 0;
+        while (in.hasNextLine()) {
+
+            String input = in.nextLine();
+            String[] split = input.split(" ");
+            switch (split[0]) {
+                case "forward":
+                    forward += Integer.parseInt(split[1]);
+                    break;
+                case "down":
+                    depth += Integer.parseInt(split[1]);
+                    break;
+                case "up":
+                    depth -= Integer.parseInt(split[1]);
+                    break;
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + split[0]);
+            }
+        }
+        System.out.println(forward);
+        System.out.println(depth);
+        double answer = forward * depth;
+        System.out.println(answer);
+    }
+}
