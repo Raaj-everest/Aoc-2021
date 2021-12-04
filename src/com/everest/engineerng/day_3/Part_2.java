cb4c7c7a18b4beca9628831bb369cbfbdb6b27df
rajesh <raaj.everest@gmail.com>
2021-12-04 10:40:32 +0530

    Added problem statement for day-4

--- src/com/everest/engineerng/day_3/Part_2.java
+++ src/com/everest/engineerng/day_3/Part_2.java
@@ -16,11 +16,11 @@ public class Part_2 {
 
         ArrayList<String> ans = new ArrayList<>();
 
-        for(int i=0;i < 12;i++) {
-            int count0 =0;
-            int count1=0;
+        for (int i = 0; i < 12; i++) {
+            int count0 = 0;
+            int count1 = 0;
             int mostCommon;
-           for(int k=0;k<list.size();k++){
+            for (int k = 0; k < list.size(); k++) {
                 String input = list.get(k);
                 String[] split = input.split("");
                 if (Integer.parseInt(split[i]) == 0) {
@@ -32,16 +32,16 @@ public class Part_2 {
             }
             System.out.println(count0);
             System.out.println(count1);
-            if(count0>count1){
-                mostCommon=0;
-            }else{
-                mostCommon=1;
+            if (count0 > count1) {
+                mostCommon = 0;
+            } else {
+                mostCommon = 1;
             }
 
 
             System.out.println(mostCommon);
-            for(int l=0;l<list.size();l++){
-                if(Integer.parseInt(String.valueOf(list.get(l).charAt(i)))==mostCommon){
+            for (int l = 0; l < list.size(); l++) {
+                if (Integer.parseInt(String.valueOf(list.get(l).charAt(i))) == mostCommon) {
                     ans.add(list.get(l));
                 }
             }
@@ -60,11 +60,11 @@ public class Part_2 {
         while (in.hasNextLine()) list.add(in.nextLine());
         in.close();
 
-        for(int i=0;i < 12;i++) {
-            int count0 =0;
-            int count1=0;
+        for (int i = 0; i < 12; i++) {
+            int count0 = 0;
+            int count1 = 0;
             int mostCommon;
-            for(int k=0;k<list.size();k++){
+            for (int k = 0; k < list.size(); k++) {
                 String input = list.get(k);
                 String[] split = input.split("");
                 if (Integer.parseInt(split[i]) == 0) {
@@ -76,17 +76,17 @@ public class Part_2 {
             }
             System.out.println(count0);
             System.out.println(count1);
-            if(count1<count0){
-                mostCommon=1;
-            }else{
-                mostCommon=0;
+            if (count1 < count0) {
+                mostCommon = 1;
+            } else {
+                mostCommon = 0;
             }
             System.out.println(mostCommon);
             if (list.size() == 1) {
                 break;
             }
-            for(int l=0;l<list.size();l++){
-                if(Integer.parseInt(String.valueOf(list.get(l).charAt(i)))==mostCommon){
+            for (int l = 0; l < list.size(); l++) {
+                if (Integer.parseInt(String.valueOf(list.get(l).charAt(i))) == mostCommon) {
                     ans.add(list.get(l));
                 }
             }
@@ -100,12 +100,6 @@ public class Part_2 {
 
         }
         System.out.println(list);
-
-
-
-
-
-
 
 
     }
